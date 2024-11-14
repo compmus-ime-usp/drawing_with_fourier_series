736f93765748c88af19266d944b904f1d5cdd449
NewbieeSaibot <tobiasrossimuller@gmail.com>
2024-11-14 00:55:35 -0300

    now we have a efficient drawer with open cv and its also drawing the circles in real time

--- drawer.py
+++ drawer.py
@@ -3,6 +3,7 @@ from path import Path
 from fourier_interpolator import FourierInterpolator1D
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 from matplotlib.animation import FuncAnimation, FFMpegWriter
 
 
@@ -23,7 +24,8 @@ class MatplotlibAnimationDrawer(Drawer):
         self.save = save
         self.margin = margin
 
-    def draw(self, path: Path, ritmo: int = 40, fps: int = 60, bitrate: int = 1800, artist: str = None) -> None:
+    def draw(self, path: Path, ritmo: int = 100, fps: int = 60, bitrate: int = 1800, artist: str = None,
+             circles: bool = False) -> None:
         fourier_interpolator = FourierInterpolator1D()
         component_sinusoid_x, component_sinusoid_y = fourier_interpolator.interpolate(path.to_complex_vector())
 
@@ -64,3 +66,82 @@ class MatplotlibAnimationDrawer(Drawer):
         if self.save:
             writer = FFMpegWriter(fps=fps, metadata=dict(artist=artist), bitrate=bitrate)
             ani.save(self.output_file, writer=writer)
+
+
+class OpenCVAnimationDrawer(Drawer):
+    def __init__(self, output_file: str, display: bool = False, save: bool = True,
+                 margin: tuple = (50, 50), frame_size: tuple = (600, 600)):
+        super().__init__()
+        self.output_file = output_file
+        self.display = display
+        self.save = save
+        self.margin = margin
+        self.frame_size = frame_size
+
+    def draw(self, path: Path, ritmo: int = 100, fps: int = 120) -> None:
+        fourier_interpolator = FourierInterpolator1D()
+        component_sinusoid_x, component_sinusoid_y = fourier_interpolator.interpolate(path.to_complex_vector())
+
+        x_min = np.min(np.sum(np.array(component_sinusoid_x), axis=0))
+        x_max = np.max(np.sum(np.array(component_sinusoid_x), axis=0))
+        y_min = np.min(np.sum(np.array(component_sinusoid_y), axis=0))
+        y_max = np.max(np.sum(np.array(component_sinusoid_y), axis=0))
+
+        scale_x = (self.frame_size[0] - 2 * self.margin[0]) / (x_max - x_min)
+        scale_y = (self.frame_size[1] - 2 * self.margin[1]) / (y_max - y_min)
+
+        writer = None
+        if self.save:
+            writer = cv2.VideoWriter(self.output_file, cv2.VideoWriter_fourcc(*'mp4v'), fps, self.frame_size)
+
+        for i in range(len(component_sinusoid_x[0])):
+            frame = np.ones((self.frame_size[1], self.frame_size[0], 3), dtype=np.uint8) * 255
+
+            # Scale and offset values
+            x_sum = np.sum(np.array(component_sinusoid_x)[:, 0:i + 1], axis=0)
+            y_sum = np.sum(np.array(component_sinusoid_y)[:, 0:i + 1], axis=0)
+            x_points = ((x_sum - x_min) * scale_x + self.margin[0]).astype(int)
+            y_points = ((y_sum - y_min) * scale_y + self.margin[1]).astype(int)
+
+            # Draw Fourier Series Circles
+            first_point = [0, 0]
+            lines_to_draw = []
+            for j in range(np.array(component_sinusoid_x).shape[0]):
+                second_point = (first_point[0] + np.array(component_sinusoid_x)[j, i],
+                                first_point[1] + np.array(component_sinusoid_y)[j, i])
+                lines_to_draw.append([first_point, second_point])
+
+                radius = int(np.sqrt(
+                    (second_point[0] - first_point[0]) ** 2 + (second_point[1] - first_point[1]) ** 2) * scale_x)
+
+                center = (int((first_point[0] - x_min) * scale_x + self.margin[0]),
+                          int((first_point[1] - y_min) * scale_y + self.margin[1]))
+                cv2.circle(frame, center, radius, (0, 255, 0), 1)
+
+                first_point = second_point
+
+            # Draw each line in lines_to_draw with scaling and margin adjustments
+            for line_start, line_end in lines_to_draw:
+                start = (int((line_start[0] - x_min) * scale_x + self.margin[0]),
+                         int((line_start[1] - y_min) * scale_y + self.margin[1]))
+                end = (int((line_end[0] - x_min) * scale_x + self.margin[0]),
+                       int((line_end[1] - y_min) * scale_y + self.margin[1]))
+                cv2.line(frame, start, end, (0, 0, 0), 2)
+
+            # Draw the Fourier approximation line up to the current frame
+            for j in range(1, len(x_points)):
+                cv2.line(frame, (x_points[j - 1], y_points[j - 1]), (x_points[j], y_points[j]), (0, 0, 255), 2)
+
+            if self.display:
+                cv2.imshow('Fourier Series Approximation Animation', cv2.flip(frame, 0))
+                if cv2.waitKey(100 // ritmo) & 0xFF == ord('q'):
+                    break
+
+            if self.save:
+                writer.write(cv2.flip(frame, 0))
+
+        if self.display:
+            cv2.destroyAllWindows()
+
+        if self.save:
+            writer.release()
--- fourier_drawer.py
+++ fourier_drawer.py
@@ -1,9 +1,9 @@
-from drawer import MatplotlibAnimationDrawer
+from drawer import MatplotlibAnimationDrawer, OpenCVAnimationDrawer
 from path import Path
 
 
 def main():
-    drawer = MatplotlibAnimationDrawer("./test.mp4")
+    drawer = OpenCVAnimationDrawer("./test.mp4")
 
     draw_path = Path.from_tuple_list([(3, 0.0), (2, 0.75), (2.3, 2), (3, 1.4), (3.7, 2), (4, 0.75)])
     drawer.draw(draw_path)
